702ce4a5963a5cba2ed9f1f2b38e8470d718e210
Connor Riva <connor.riva@appfolio.com>
2022-06-26 21:42:04 -0400

    Switch to using flask-sqlalchemy
    After consideration, it was decided that a relational database would be better
    suited for the data structure of this app, so we've switched to using mysql

--- db.py
+++ db.py
@@ -1,11 +1,14 @@
 from flask import current_app, g
 from flask.cli import with_appcontext
-from flask_pymongo import PyMongo
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 def get_db():
     if 'db' not in g:
-        current_app.config['MONGO_URI'] = "mongodb://localhost:27017/companion_planting"
-        g.db = PyMongo(current_app)
+        current_app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/garden_companion'
+        sqlalchemy = SQLAlchemy(current_app)
+        Migrate(current_app, sqlalchemy)
+        g.db = sqlalchemy
 
     return g.db
