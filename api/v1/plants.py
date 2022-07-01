288f4d09031332e47159a4fb45db73f824fa372c
Connor Riva <connor.riva@appfolio.com>
2022-07-01 04:44:55 -0400

    Update plant api to use new slqalchemy setup
    This commit updates the plant api in order to make use of the new db models set
    up with sqlalchemy to run queries on the db tables.

--- api/v1/plants.py
+++ api/v1/plants.py
@@ -1,15 +1,17 @@
 from flask import Blueprint
-from flask.cli import with_appcontext
 from bson import json_util
 from flask_restful import Api, Resource
-from garden_companion_api import db
+from garden_companion_api.database import SessionLocal, engine
+from garden_companion_api.db.models.Plant import Plant as PlantModel
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
+session = SessionLocal()
+
 
 class Plant(Resource):
     def get(self):
-        plants = db.get_db().db.plants.find()
+        plants = session.query(PlantModel).all()
         print(plants)
         return json_util.dumps(plants)
